e74a42cd25ccc642f9aa8cb1390116eb7d1e8807
derek-alves <derek.alves@fatec.sp.gov.br>
2020-11-21 19:08:01 -0300

    Default structure

---
+++ Next_Ts_Styled/src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+import * as S from "./styles";
+
+const Main: React.FC = () => (
+  <S.Wrapper>
+    <S.Title>Default structure</S.Title>
+    <S.Description>TypeScript, NextJS e Styled Components</S.Description>
+  </S.Wrapper>
+);
+export default Main;
